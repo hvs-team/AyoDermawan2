6c47ead3c1c8ddc136a15993998ae74963e7463f
nuhsat <nuhsatria@gmail.com>
2017-09-09 01:33:07 +0700

    9 September 2017, 01:33

--- src/pages/donatur-lelang2/donatur-lelang2.ts
+++ src/pages/donatur-lelang2/donatur-lelang2.ts
@@ -4,6 +4,12 @@ import { IonicPage, NavController, NavParams, LoadingController, AlertController
 
 import { TabsDonaturPage } from '../tabs-donatur/tabs-donatur';
 
+import { Data } from '../../providers/data';
+import { Http } from '@angular/http';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 // @IonicPage()
 @Component({
   selector: 'page-donatur-lelang2',
@@ -21,14 +27,23 @@ export class DonaturLelang2Page {
   address: string;
   description: string;
 
+  id_lembaga: string;
+
   constructor(
+    private fireauth: AngularFireAuth, 
+    private firedata: AngularFireDatabase, 
+    public http: Http, 
+    public data: Data,
     public navCtrl: NavController, 
     public navParams: NavParams,
     public loadCtrl: LoadingController,
     public alertCtrl: AlertController,
     ) {
+      this.data.getDataLembaga().then((data) => {
+        this.id_lembaga = data.id;
+     })
 
-    let dataBarang = JSON.parse(this.navParams.data);
+     let dataBarang = JSON.parse(this.navParams.data);
 
 
       this.name = dataBarang.name;
@@ -39,6 +54,8 @@ export class DonaturLelang2Page {
       this.kota = dataBarang.kota;
       this.kecamatan = dataBarang.kecamatan;
       this.address = dataBarang.address;
+      this.description = dataBarang.description;
+      
 
   }
 
@@ -63,7 +80,19 @@ export class DonaturLelang2Page {
     loading.present();
 
     //tempat firebase
-
+    this.firedata.list('/lelang/'+ this.id_lembaga).push({ 
+      nama: this.name,
+      price: this.price,
+      kategori: this.kategori,
+      lembaga_barang: this.lembaga_barang,
+      provinsi: this.provinsi,
+      kota: this.kota,
+      kecamatan: this.kecamatan,
+      address: this.address,
+      description: this.description,
+      notif: 1, //tertunda
+      keterangan: "Menunggu Persetujuan"
+    });
     //
 
     setTimeout(() => {
